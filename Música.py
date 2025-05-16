303911ec229f09771f3f8b62da2cb0514475973c
jfellipe77 <110146640+jfellipe77@users.noreply.github.com>
2025-05-16 19:39:54 -0300

    Add files via upload

---
+++ Música.py
@@ -0,0 +1,16 @@
+listamusicas=[]
+
+while True:
+
+    música = input ("Digite sua música: ")
+    cantor = input ("Digite o cantor da música: ")
+    estilomusical = input ("Digite o estilo da música: ")
+
+    usuário = {"Música": música, "cantor": cantor, "estilomusical": estilomusical}
+
+    listamusicas.append(usuário)
+
+    print(usuário)
+
+    for i in listamusicas
+print("Você escolheu", op)
